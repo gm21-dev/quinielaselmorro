f49a49a03faff70ba3381e5ef502c6c0171aea26
GerardoMG-dev <gerardo.montes.dev@gmail.com>
2026-08-01 09:07:52 -0600

    Fix: Configurar base href dinamico y creación del 404.html

--- quinielas/obj/Debug/net10.0/quinielas.AssemblyInfo.cs
+++ quinielas/obj/Debug/net10.0/quinielas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quinielas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a669e214170c973a1a019bd6055f35047cb00a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d377ffb076889c3317481390249408c2c30c10")]
 [assembly: System.Reflection.AssemblyProductAttribute("quinielas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quinielas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
